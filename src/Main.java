9da22bf5f93ccdea05c960aed50344d1af1300e5
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-04-01 23:18:25 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,13 @@ import java.util.*;
 public class Main {
     public static void main(String[] args)  {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        List<String> text = new ArrayList<>(List.of("В", "консоль", "выводятся", "только", "уникальные", "слова",
+         "из", "списка", "слов", "Код", "работает", "с", "любой", "последовательностью", "и", "объемом", "списка", "слов"));
+
         task1(nums);
         task2(nums);
+        task3(text);
+        task4(text);
     }
 
     public static void task1(List<Integer> nums) {
@@ -29,6 +34,31 @@ public class Main {
             }
         }
         numbers.sort(Comparator.naturalOrder());
+        System.out.println(numbers);
+    }
+
+    public static void task3(List<String> text) {
+        System.out.println("Задание 3");
+        Set<String> textSet = new HashSet<>(List.copyOf(text));
+        System.out.println(textSet);
+    }
+
+    public static void task4(List<String> text) {
+        System.out.println("Задание 4");
+        Integer num;
+        List<Integer> numbers = new ArrayList<>();
+        Set<String> textSet = new HashSet<>(List.copyOf(text));
+        for (String strSet : textSet) {
+            num = 0;
+            for (String str : text) {
+                if (strSet.equals(str)) {
+                    num ++;
+                }
+            }
+            if (num > 1) {
+                numbers.add(num);
+            }
+        }
         System.out.println(numbers);
     }
 }
